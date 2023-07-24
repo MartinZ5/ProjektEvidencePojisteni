a2ad42e7eb95c500a3707f7ed1c4f952ba8c11af
MartinZ5 <139696186+MartinZ5@users.noreply.github.com>
2023-07-24 23:35:09 +0200

    Add files via upload

---
+++ InsuranceListProject/DatabazeP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceListProject
+{
+    class DatabazeP
+    {
+        private List<Zaznam> _zaznamy { get; set; } = new List<Zaznam>();
+
+        public void PridejZaznam(Zaznam zaznam)
+        {
+            _zaznamy.Add(zaznam);
+        }
+
+        public void VypisVsechny()
+        {
+            foreach(var zaznam in _zaznamy) 
+            {
+                Console.WriteLine($"Zaznam: {zaznam.Jmeno}, {zaznam.Prijmeni}, {zaznam.Vek}, {zaznam.TelCislo}");
+            }
+        }
+
+        public void VypisOdpovidajici(string hledaneJmeno, string hledanePrijmeni)
+        {
+
+            var odpovidajiciZaznamy = _zaznamy.Where(z => z.Jmeno == hledaneJmeno && z.Prijmeni == hledanePrijmeni).ToList();
+
+            if (odpovidajiciZaznamy != null)
+            {
+                foreach (var zaznam in odpovidajiciZaznamy)
+                {
+                    Console.WriteLine($"Zaznam: {zaznam.Jmeno}, {zaznam.Prijmeni}, {zaznam.Vek}, {zaznam.TelCislo}");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ InsuranceListProject/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Concurrent;
+
+namespace InsuranceListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Evidence Pojištěných");
+            Console.WriteLine("--------------------------------");
+
+            DatabazeP databazeP = new DatabazeP();
+            char volba = '0';
+            //hlavní cyklus
+            while (volba != '4')
+            {
+                Console.WriteLine();
+                Console.WriteLine("Vyberte si akci:");
+                Console.WriteLine("1 - Přidat nového pojištěného");
+                Console.WriteLine("2 - Vypsat všechny pojištěné");
+                Console.WriteLine("3 - Vyhledat pojištěného(dle jména a příjmení)");
+                Console.WriteLine("4 - Konec");
+
+                volba = Console.ReadKey().KeyChar;
+                 Console.WriteLine();
+                 //reakce na volbu
+                 switch(volba)
+                 {
+                     case '1':
+                         Console.WriteLine("Zadej jméno:");
+                        string jmeno = Console.ReadLine().Trim();
+                        while(string.IsNullOrWhiteSpace(jmeno))
+                        {
+                            Console.WriteLine("Zadej text znovu:");
+                            jmeno = Console.ReadLine().Trim();
+                        }
+                         Console.WriteLine("Zadej příjmení:");
+                        string prijmeni = Console.ReadLine().Trim();
+                        while (string.IsNullOrWhiteSpace(prijmeni))
+                        {
+                            Console.WriteLine("Zadej text znovu:");
+                            prijmeni = Console.ReadLine().Trim();
+                        }
+                        Console.WriteLine("Zadej věk:");
+                         int vek = int.Parse(Console.ReadLine().Trim());
+                        Console.WriteLine("Telefonní číslo:");
+                         string telCislo = Console.ReadLine().Trim();
+
+                         var novyZaznam = new Zaznam(jmeno, prijmeni, vek, telCislo);
+                         databazeP.PridejZaznam(novyZaznam);
+                         break;
+
+                     case '2':
+                        databazeP.VypisVsechny();
+                         break;
+
+                     case '3':
+                        Console.WriteLine("Jaké jméno hledáš?");
+                        string hledaneJmeno = Console.ReadLine();  
+                        Console.WriteLine("Příjmení:");
+                        string hledanePrijmeni = Console.ReadLine();
+                        databazeP.VypisOdpovidajici(hledaneJmeno, hledanePrijmeni);
+                         break;
+
+                     case '4':
+                        Console.WriteLine("Libovolnou klávesou ukončíte program...");
+                         break;
+
+                     default:
+                         Console.WriteLine("Neplatná volba, stiskněte libovolnou klávesu a opakujte volbu.");
+                         break; 
+                 }
+             }
+        }
+    }
+}
